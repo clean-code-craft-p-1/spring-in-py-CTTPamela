18d72afec81bb7cf375aac23bf206e54c083c3a2
CTTPamela <pamela.chang@philips.com>
2025-07-14 16:30:21 +0800

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -3,7 +3,7 @@ def calculateStats(numbers):
     if not numbers:
         return {}
     return {
-        'avg': sum(numbers) / len(numbers),
-        'min': min(numbers),
-        'max': max(numbers)
+        "avg": sum(numbers) / len(numbers),
+        "min": min(numbers),
+        "max": max(numbers)
     }
